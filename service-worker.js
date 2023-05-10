cf01f30866fac5c8c03292f32deada48061829bc
afifurrohman-id <afifurrohman.id@gmail.com>
2023-05-10 17:42:21 +0700

    update service worker & manifest

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,27 @@
-
 self.addEventListener('install', (event) => event.waitUntil(registerCache()))
 self.addEventListener('fetch', (event) => event.respondWith(handleCache(event)))
 
-async function registerCache() {
+async function openCache() {
   const cacheAfif = `afif-web-cache`
   const cache = await caches.open(cacheAfif)
+  return cache
+}
+async function registerCache() {
+  const cache = await openCache()
   const filesToCache = ['/', '/scripts/main.js', '/styles/index.css']
-  console.info('%c Service worker caching all', 'font-weight:bold;color:green')
+  console.info('%c Service worker caching all', 'font-weight: bold; color: lightgreen;')
   await cache.addAll(filesToCache)
 }
 
 async function handleCache(networkEvent) {
-  const cachedResponse = await caches.match(networkEvent.request)
-  const networkResponse = fetch(networkEvent.request)
-  return cachedResponse || networkResponse
-
+  try {
+    console.info('%c Service Worker Trying Fetch from Network', 'font-weight: bold; color: blue;')
+    const networkResponse = await fetch(networkEvent.request)
+    return networkResponse
+  } catch (error) {
+    console.info(`%c ${error} | Service Worker using cache`, 'font-weight: bold; color: yellow')
+    const cache = await openCache()
+    const cachedResponse = await cache.match(networkEvent.request)
+    return cachedResponse
+  }
 }
